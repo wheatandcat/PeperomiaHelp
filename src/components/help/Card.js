1f2f9fd78cb560e079e797ff896b36b41979589d
Yohei Iino <wheatandcat@gmail.com>
2020-07-19 20:15:27 +0900

    fix layout for sp (#6)

--- src/components/help/Card.js
+++ src/components/help/Card.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { makeStyles } from "@material-ui/core/styles"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 
 const useStyles = makeStyles(() => ({
   card: {
@@ -10,13 +11,20 @@ const useStyles = makeStyles(() => ({
     padding: "1rem",
     margin: "1rem 0",
   },
+  cardSP: {
+    backgroundColor: "#fff",
+    padding: "0 0.1rem",
+    margin: "1rem 0",
+    borderRadius: "0",
+  },
 }))
 
 const HelpCard = props => {
   const classes = useStyles()
+  const matches = useMediaQuery("(max-width:767px)")
 
   return (
-    <Card className={classes.card}>
+    <Card className={matches ? classes.cardSP : classes.card}>
       <CardContent>{props.children}</CardContent>
     </Card>
   )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import useMediaQuery from "@material-ui/core/useMediaQuery"
 import Header from "./header"
 import "./layout.css"
 
@@ -23,16 +16,23 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const matches = useMediaQuery("(max-width:767px)")
+
+  const rootStyle = {
+    margin: `0 auto`,
+    maxWidth: 960,
+    padding: `0 1.0875rem 1.45rem`,
+  }
+
+  const rootStyleSP = {
+    margin: `0`,
+    padding: `0`,
+  }
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div style={matches ? rootStyleSP : rootStyle}>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
